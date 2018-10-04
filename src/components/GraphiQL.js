874b4511914536b0ccae78131b930f0455dbf2d7
fred wang <51wxk@sina.com>
2018-10-04 15:56:37 +0800

    add fetcher

--- src/components/GraphiQL.js
+++ src/components/GraphiQL.js
@@ -65,6 +65,19 @@ export class GraphiQL extends React.Component {
     ResultsTooltip: PropTypes.any,
   };
 
+/**
+*
+* function graphQLFetcher(graphQLParams) {
+*   return fetch(window.location.origin + '/graphql', {
+*     method: 'post',
+*     headers: { 'Content-Type': 'application/json' },
+*     body: JSON.stringify(graphQLParams),
+*   }).then(response => response.json());
+* }
+**/
+
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.body);
+
   constructor(props) {
     super(props);
 
